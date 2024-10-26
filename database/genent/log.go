6f56eb7e109ed8000760047a88f215e859f23fe7
anesthetised <methoxieticyclidine@pm.me>
2024-10-26 23:26:41 +0300

    docs(database/genent): document LogFn

--- database/genent/log.go
+++ database/genent/log.go
@@ -2,6 +2,8 @@ package genent
 
 import "log/slog"
 
+// LogFn returns an Ent-compatible logging
+// wrapper function for slog..
 func LogFn(logger *slog.Logger) func(...any) {
 	return func(es ...any) {
 		for _, e := range es {
